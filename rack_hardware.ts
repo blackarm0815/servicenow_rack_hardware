a5d846526bc2ec5826e8283b4aa1e593f80afb86
blackarm0815 <honungsbjorn@protonmail.com>
2023-03-06 20:48:43 +0000

    tidied up rack sys_ids

--- rack_hardware.ts
+++ rack_hardware.ts
@@ -554,7 +554,18 @@ const redbeardRackHardwareSort = (
     usageUnits,
   };
 };
-const testRackSysIds = ['bc22df4adb1ec70cab79f7d41d9619f6', 'b817db4edb168bc010b6f1561d961914', 'f4738c21dbb1c7442b56541adc96196a', 'b1c34461dbb1c7442b56541adc96198f', 'efd3cc61dbb1c7442b56541adc961978', 'bdba2b74db271788259e5898dc9619a4', '3abaa3f4db271788259e5898dc9619ab', '3bba63f4db271788259e5898dc961971', '30cae3f4db271788259e5898dc961926', '0aca67f4db271788259e5898dc961979'];
+const testRackSysIds = [
+  'bc22df4adb1ec70cab79f7d41d9619f6',
+  'b817db4edb168bc010b6f1561d961914',
+  'f4738c21dbb1c7442b56541adc96196a',
+  'b1c34461dbb1c7442b56541adc96198f',
+  'efd3cc61dbb1c7442b56541adc961978',
+  'bdba2b74db271788259e5898dc9619a4',
+  '3abaa3f4db271788259e5898dc9619ab',
+  '3bba63f4db271788259e5898dc961971',
+  '30cae3f4db271788259e5898dc961926',
+  '0aca67f4db271788259e5898dc961979',
+];
 const results: RackHardwareSortResult = redbeardRackHardwareSort(testRackSysIds);
 // @ts-ignore
 gs.print(results);
